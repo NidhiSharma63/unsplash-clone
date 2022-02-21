695baa571b9b2af442cd41bfde27e2e4ea2a1e1c
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-02-21 13:10:34 +0530

    api fixed

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+
+function getimage() {
+    url = 'https://api.unsplash.com/search/photos?query=' + input.value + '&per_page=30&client_id=5OXcnxdQpZLtAG0_jRNpqEQhTlUOQL3TKviFAUbBKm8',
+        fetch(url).then((response) => {
+            return response.json()
+        }).then((data) => {
+            data.results.forEach(element => {
+                let srorce = element.urls.regular
+                console.log(srorce)
+                let Div = document.createElement("Div");
+                Div.setAttribute("class", "imgDiv");
+                let img = document.createElement("img");
+                img.src = `${element.urls.regular}`
+                Div.appendChild(img);
+                document.querySelector(".imageContainer").appendChild(Div)
+            });
+        })
+}
