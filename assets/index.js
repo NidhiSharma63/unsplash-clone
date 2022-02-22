1853d33127dc3ce011417e058a9963970589c783
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-02-22 13:36:35 +0530

    popup image added

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 // console.log()
 let imageContainer = document.querySelector(".imageContainer");
+let imgShowed = document.querySelector(".imgShowed");
 const imageArr = [
   "./assets/images/img1.jpeg",
   "./assets/images/img2.jpeg",
@@ -13,22 +14,22 @@ const imageArr = [
   "./assets/images/img10.jpeg",
 ];
 function loadImage() {
-  for(let i=0;i<imageArr.length;i++){
+  for (let i = 0; i < imageArr.length; i++) {
     let Div = document.createElement("div");
     Div.setAttribute("class", "imgDiv");
     let imgtag = document.createElement("img");
     imgtag.src = imageArr[i];
     Div.appendChild(imgtag);
-    imageContainer.appendChild(Div)
+    imageContainer.appendChild(Div);
   }
 }
-window.onload=loadImage()
+window.onload = loadImage();
 function getimage() {
-    imageContainer.innerHTML='';
+  imageContainer.innerHTML = "";
   (url =
     "https://api.unsplash.com/search/photos?query=" +
     input.value +
-    "&per_page=130&client_id=5OXcnxdQpZLtAG0_jRNpqEQhTlUOQL3TKviFAUbBKm8"),
+    "&per_page=20&client_id=5OXcnxdQpZLtAG0_jRNpqEQhTlUOQL3TKviFAUbBKm8"),
     fetch(url)
       .then((response) => {
         return response.json();
@@ -36,13 +37,22 @@ function getimage() {
       .then((data) => {
         data.results.forEach((element) => {
           let srorce = element.urls.regular;
-          console.log(srorce);
           let Div = document.createElement("Div");
           Div.setAttribute("class", "imgDiv");
           let img = document.createElement("img");
           img.src = `${element.urls.regular}`;
           Div.appendChild(img);
           document.querySelector(".imageContainer").appendChild(Div);
+          let imgDiv = document.querySelectorAll(".imgDiv");
+          imgDiv.forEach(element => {
+            element.addEventListener("click",(e)=>{
+              let target = e.currentTarget
+              let clickedimagesrc = target.firstChild.src
+              imgShowed.innerHTML=`<img src="${clickedimagesrc}" class="imgfit" alt="...">`
+              // <img src="..." class="img-fluid" alt="...">
+             
+            })
+          });
         });
       });
 }
